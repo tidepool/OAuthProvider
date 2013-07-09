2715e3304883134209e73c80bff58599746f054b
Kerem Karatal <kkaratal@tidepool.co>
2013-07-09 11:17:15 -0700

    Emotion test just passes the template definition back instead of trying to create image urls for the images referenced.

--- app/models/generators/emotions_circles_generator.rb
+++ app/models/generators/emotions_circles_generator.rb
@@ -4,27 +4,28 @@ class EmotionsCirclesGenerator
   end
 
   def generate
-    result = {}
-    result["friendly_name"] = @stage_template["friendly_name"]
-    result["instructions"] = @stage_template["instructions"]
-    result["view_name"] = @stage_template["view_name"]
+    # result = {}
+    # result["friendly_name"] = @stage_template["friendly_name"]
+    # result["instructions"] = @stage_template["instructions"]
+    # result["view_name"] = @stage_template["view_name"]
 
-    image_sequence = []
-    circle_sequence = @stage_template["circles"]
-    circle_sequence.each do |image|
-      if (Rails.env.production?)
-        # TODO: S3 based location for the images
-        image_url = "/images/emotions_images/#{image["image_id"]}.jpg"
-      else
-        image_url = "/images/emotions_images/#{image["image_id"]}.jpg"
-      end        
+    # image_sequence = []
+    # circle_sequence = @stage_template["circles"]
+    # circle_sequence.each do |image|
+    #   if (Rails.env.production?)
+    #     # TODO: S3 based location for the images
+    #     image_url = "/images/emotions_images/#{image["image_id"]}.jpg"
+    #   else
+    #     image_url = "/images/emotions_images/#{image["image_id"]}.jpg"
+    #   end        
 
-      image_sequence << { trait: image["trait"], 
-        image_id: image["image_id"], 
-        url: image_url
-      }
-    end
-    result["circles"] = image_sequence
-    result
+    #   image_sequence << { trait: image["trait"], 
+    #     image_id: image["image_id"], 
+    #     url: image_url
+    #   }
+    # end
+    # result["circles"] = image_sequence
+    # result
+    @stage_template
   end
 end
