676853b2cd1ee421546ab363dc0180e51f85dfaa
Kerem Karatal <kkaratal@tidepool.co>
2013-07-09 11:11:37 -0700

    Changes the name for user_events to emotions_circles and changes the order of emotions in the emotions game

--- spec/api/v1/recommendations_spec.rb
+++ spec/api/v1/recommendations_spec.rb
@@ -115,7 +115,7 @@ describe 'Recommendations API' do
     response.status.should == 200
     reco_result = JSON.parse(response.body, symbolize_names: true)
     reco_result[:emotion].should == 'awe'
-
+    reco_result[:friendly_name].should == 'Awe'
   end
 
 end
