7045d6d8304eb2b8dc9613273f4e2c1d69fa9d85
vagrant <vagrant@precise64.(none)>
2013-07-16 02:49:02 +0000

    Build 50 combined for deployment

--- public/require_config.js
+++ public/require_config.js
@@ -1,13 +1,14 @@
 
 define([], function() {
   return require.config({
-    packages: ['core', 'game/levels/_base', 'game/levels/reaction_time_disc', 'game/levels/rank_images', 'game/levels/circle_size', 'game/levels/circle_proximity', 'game/levels/alex_trebek', 'dashboard/widgets/base', 'dashboard/widgets/lister', 'ui_widgets/formation'],
+    packages: ['core', 'game/levels/_base', 'game/levels/reaction_time_disc', 'game/levels/rank_images', 'game/levels/circle_size', 'game/levels/circle_proximity', 'game/levels/alex_trebek', 'game/levels/emotions_circles', 'dashboard/widgets/base', 'dashboard/widgets/lister', 'ui_widgets/formation'],
     paths: {
+      jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
+      bootstrap: '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min',
       Handlebars: "bower_components/require-handlebars-plugin/Handlebars",
       underscore: "bower_components/underscore-amd/underscore",
-      jquery: "bower_components/jquery/jquery",
       jqueryui: "bower_components/jquery-ui/jqueryui",
-      jquiTouchPunch: 'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch',
+      jquiTouchPunch: 'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min',
       backbone: "bower_components/backbone-amd/backbone",
       syphon: 'bower_components/tidepool-backbone.syphon/lib/amd/backbone.syphon',
       text: "bower_components/requirejs-text/text",
@@ -15,15 +16,14 @@ define([], function() {
       chart: "bower_components/Chart.js/Chart.min",
       markdown: 'bower_components/markdown/lib/markdown',
       nested_view: "scripts/vendor/nested_view",
-      bootstrap: "scripts/vendor/bootstrap",
       results: "scripts/views/results",
       controllers: "scripts/controllers",
       helpers: "scripts/helpers"
     },
     shim: {
       bootstrap: {
-        deps: ["jquery"],
-        exports: "jquery"
+        deps: ['jquery'],
+        exports: 'jquery'
       },
       chart: {
         exports: "Chart"
@@ -31,6 +31,7 @@ define([], function() {
       markdown: {
         exports: 'markdown'
       }
-    }
+    },
+    waitSeconds: 14
   });
 });
