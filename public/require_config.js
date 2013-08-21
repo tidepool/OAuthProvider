b812600a826fa765469ad81bcc2e0f11adce4bdb
vagrant <vagrant@precise64.(none)>
2013-08-21 01:34:50 +0000

    Build 61 combined for deployment

--- public/require_config.js
+++ public/require_config.js
@@ -1,24 +1,25 @@
 
 define([], function() {
   return require.config({
-    packages: ['core', 'game/levels/_base', 'game/levels/reaction_time_disc', 'game/levels/rank_images', 'game/levels/circle_size', 'game/levels/circle_proximity', 'game/levels/alex_trebek', 'game/levels/emotions_circles', 'game/levels/snoozer', 'dashboard/widgets/base', 'dashboard/widgets/lister', 'ui_widgets/formation'],
+    packages: ['core', 'game/levels/_base', 'game/levels/reaction_time_disc', 'game/levels/rank_images', 'game/levels/circle_size', 'game/levels/person_fill', 'game/levels/proximity_takes_turns', 'game/levels/circle_proximity', 'game/levels/alex_trebek', 'game/levels/emotions_circles', 'game/levels/snoozer', 'game/levels/interest_picker', 'dashboard/widgets/base', 'dashboard/widgets/lister', 'ui_widgets/formation'],
     paths: {
       jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
       bootstrap: '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min',
-      Handlebars: "bower_components/require-handlebars-plugin/Handlebars",
-      underscore: "bower_components/underscore-amd/underscore",
-      jqueryui: "bower_components/jquery-ui/jqueryui",
+      Handlebars: 'bower_components/require-handlebars-plugin/Handlebars',
+      underscore: 'bower_components/underscore-amd/underscore',
+      jqueryui: 'bower_components/jquery-ui/jqueryui',
       jquiTouchPunch: 'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min',
-      backbone: "bower_components/backbone-amd/backbone",
+      backbone: 'bower_components/backbone-amd/backbone',
       syphon: 'bower_components/tidepool-backbone.syphon/lib/amd/backbone.syphon',
-      text: "bower_components/requirejs-text/text",
-      toastr: "bower_components/toastr",
-      chart: "bower_components/Chart.js/Chart.min",
+      text: 'bower_components/requirejs-text/text',
+      toastr: 'bower_components/toastr',
+      chart: 'bower_components/Chart.js/Chart.min',
       markdown: 'bower_components/markdown/lib/markdown',
-      nested_view: "scripts/vendor/nested_view",
-      results: "scripts/views/results",
-      controllers: "scripts/controllers",
-      helpers: "scripts/helpers"
+      fastclick: 'bower_components/fastclick/lib/fastclick',
+      nested_view: 'scripts/vendor/nested_view',
+      results: 'scripts/views/results',
+      controllers: 'scripts/controllers',
+      helpers: 'scripts/helpers'
     },
     shim: {
       bootstrap: {
@@ -26,7 +27,7 @@ define([], function() {
         exports: 'jquery'
       },
       chart: {
-        exports: "Chart"
+        exports: 'Chart'
       },
       markdown: {
         exports: 'markdown'
