679e81fafd1e324039ec5fb297dabd59399911fa
Kerem Karatal <kkaratal@tidepool.co>
2013-09-06 19:30:38 -0700

    Updated blitz tests.

--- lib/blitz/create_user.rb
+++ lib/blitz/create_user.rb
@@ -2,3 +2,51 @@ require 'blitz'
 require 'pp'
 require 'pry'
 
+def server_endpoint
+  # "https://tide-dev.herokuapp.com"
+  "https://api.tidepool.co"  
+end
+
+def user_token
+  # "cbe52a989f38125da0c2df5bc3c13c7bc0dbbe381c3e940c4bcae3997eb638c9"  # Tide-dev
+  "f2c298e0f99099166013e7796b5419b1546d03ba7ff499444fc61f9f0ff8e389"  # Production
+end
+
+def setup_test
+  test = {
+    "steps" => [
+      {
+        "timeout" => 2000,
+        "request" => "POST",
+        "headers" => ["Content-Type: application/json",
+          "Authorization: Bearer #{user_token}"
+          ],
+        "content" => {
+          "data" => ["{\"def_id\":\"snoozer\"}"]
+          },
+        "url" => "#{server_endpoint}/api/v1/users/-/games.json"
+      }
+    ],
+    "region" => "california"      
+  }
+end
+
+
+def setup_test2
+  {
+    "steps" => [
+      {
+        "timeout" => 2000,
+        "request" => "GET",
+        "headers" => ["Content-Type: application/json",
+          "Authorization: Bearer #{user_token}"
+          ],
+        "content" => {
+          "data" => []
+          },
+        "url" => "#{server_endpoint}/api/v1/users/-/games/\#{gameid}/results.json"
+      }
+    ],
+    "region" => "california"      
+  }
+end
--- lib/tasks/create_test_data.rake
+++ lib/tasks/create_test_data.rake
@@ -1,9 +1,11 @@
+require File.expand_path('../../blitz/create_user.rb', __FILE__)
+
 namespace :loadtests do 
   task :create_test_data, [:test_type] => :environment do |t, args|
     client_id = "3e372449d494eb6dc7d74cd3da1d6eedd50c7d98f3dedf1caf02960a9a260fb1"
     client_secret = "3e4da2177beee0d8ec458480526b3716047b3ff0df3362262183f6841253a706"
 
-    client = OAuth2::Client.new(client_id, client_secret, raise_errors: false, site: "https://tide-dev.herokuapp.com")
+    client = OAuth2::Client.new(client_id, client_secret, raise_errors: false, site: server_endpoint)
     token = client.password.get_token("pp@kk.com", "12345678")
     puts "User Token: #{token.token}"
     # response = token.get("/api/v1/users/-.json")
@@ -15,7 +17,7 @@ namespace :loadtests do
     events_json = IO.read(File.expand_path("../../analyze/spec/fixtures/#{filename}", __FILE__))
     events = JSON.parse(events_json)
 
-    (0..50).each do |i|
+    (0..250).each do |i|
       response = token.post("/api/v1/users/-/games.json",
         { body: { def_id: 'snoozer' } })
       result = JSON.parse(response.body, symbolize_names: true)
