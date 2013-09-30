91912a5897b8f5f869ad37bccf00c851db9c8fae
Kerem Karatal <kkaratal@tidepool.co>
2013-09-30 13:47:20 -0700

    Changes so that we can create user data based game generators. Adds the snoozer last 5 games average results calculation.

--- app/models/game.rb
+++ app/models/game.rb
@@ -38,7 +38,7 @@ class Game < ActiveRecord::Base
     create! do |game|
       game.definition = definition
       game.name = definition.unique_name  # Denormolizing the unique_name of the definition
-      game.stages = definition.stages_from_stage_definition
+      game.stages = definition.stages_from_stage_definition(target_user)
       game.user = target_user
       game.calling_ip = calling_ip
       game.date_taken = Time.zone.now # Always use Time.zone not Time
--- app/models/generators/survey_generator.rb
+++ app/models/generators/survey_generator.rb
@@ -1,9 +1,2 @@
-class SurveyGenerator
-  def initialize(stage_template)
-    @stage_template = stage_template
-  end
-
-  def generate
-    @stage_template
-  end
+class SurveyGenerator < BaseGenerator
 end
