b6abe7e95291b7af65af7ca0e44715c7f0b91cd0
Kerem Karatal <kkaratal@tidepool.co>
2013-10-04 15:45:12 -0700

    Fixes and adds more permissions for jawbone

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :fitbit, ENV['FITBIT_KEY'], ENV['FITBIT_SECRET'], :setup => lambda{|env| env['omniauth.strategy'].options[:authorize_params] = { :display => 'touch'} }
   provider :jawbone, ENV['JAWBONE_KEY'], ENV['JAWBONE_SECRET'],
-            :scope => ['extended_read', 'move_read', 'sleep_read', 'meal_read', 'weight_read']
+            :scope => 'extended_read location_read friends_read mood_read move_read sleep_read meal_read weight_read cardiac_read generic_event_read'
 end
