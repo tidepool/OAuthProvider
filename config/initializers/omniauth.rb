89e9c5871a5d9b6084da743b9dfcd56d789fadee
Kerem Karatal <kkaratal@tidepool.co>
2013-10-07 14:05:59 -0700

    Add all to jawbone authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :fitbit, ENV['FITBIT_KEY'], ENV['FITBIT_SECRET'], :setup => lambda{|env| env['omniauth.strategy'].options[:authorize_params] = { :display => 'touch'} }
   provider :jawbone, ENV['JAWBONE_KEY'], ENV['JAWBONE_SECRET'],
-            :scope => 'extended_read location_read friends_read mood_read move_read sleep_read meal_read weight_read cardiac_read generic_event_read'
+            :scope => 'extended_read location_read friends_read mood_read mood_write move_read move_write sleep_read sleep_write meal_read meal_write weight_read weight_write cardiac_read cardiac_write generic_event_read generic_event_write'
 end
