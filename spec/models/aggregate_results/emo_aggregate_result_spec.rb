32bb5729d8ebb3cb7bd23459af1952029b04a8a1
Kerem Karatal <kkaratal@tidepool.co>
2013-10-29 11:35:20 -0700

    Game name is required and some tests were failing because the factories do not provide game name

--- spec/models/aggregate_results/emo_aggregate_result_spec.rb
+++ spec/models/aggregate_results/emo_aggregate_result_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EmoAggregateResult do
   let(:user) { create(:user) }
-  let(:game) { create(:game, user: user) }
+  let(:game) { create(:game, user: user, name: 'faceoff') }
 
 
 end
